84df1d42e650285a97bb8c2650c8699605eada0e
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2025-11-15 23:22:17 -0600

    fix: LSP highlight
    Manually changed the highlight color, used this PR for reference until
    it is merged into main:
    https://github.com/olivercederborg/poimandres.nvim/pull/46

--- neovim.lua
+++ neovim.lua
@@ -1,21 +1,28 @@
 return {
-  {
-    "olivercederborg/poimandres.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("poimandres").setup({
-        -- leave this setup function empty for default config
-        -- or refer to the configuration section
-        -- for configuration options
-      })
-    end,
-  },
+	{
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local p = require("poimandres.palette")
+			require("poimandres").setup({
+				-- leave this setup function empty for default config
+				-- or refer to the configuration section
+				-- for configuration options
+				-- Fix taken from: https://github.com/olivercederborg/poimandres.nvim/pull/46
+				highlight_groups = {
+					LspReferenceText = { link = "Visual" },
+					LspReferenceRead = { link = "Visual" },
+					LspReferenceWrite = { link = "Visual" },
+				},
+			})
+		end,
+	},
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "poimandres",
-    },
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "poimandres",
+		},
+	},
 }
